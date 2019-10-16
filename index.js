cdfcc3ae1bb3fbffe010a745264c2837013d16b1
snowdreams1006 <snowdreams1006@163.com>
2019-10-16 09:31:17 +0800

    id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 hexo.extend.filter.register('after_post_render', function(data) {
 	var gitalkConfig = hexo.config.plugins.gitalk;
-	if (gitalkConfig) {		
+	if (gitalkConfig) {
+		gitalkConfig.id = data._id;
+
 		data.content = data.content + '\n<div id="gitalk-container"></div>\n<link rel="stylesheet" href="https://unpkg.com/gitalk/dist/gitalk.css"><script src="https://unpkg.com/gitalk/dist/gitalk.min.js"></script>';
-
-		var str = '<script>new Gitalk('+JSON.stringify(gitalkConfig)+').render("gitalk-container")'+'</script>';
-
+		var str = '<script>new Gitalk(' + JSON.stringify(gitalkConfig) + ').render("gitalk-container")' + '</script>';
 		data.content = data.content + '\n' + str;
 	}
 

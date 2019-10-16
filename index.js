d48cf8f7f5fdda59cf5fae988f1f713862801545
snowdreams1006 <snowdreams1006@163.com>
2019-10-16 13:50:57 +0800

    change id to pathname

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 hexo.extend.filter.register('after_post_render', function(data) {
 	var gitalkConfig = hexo.config.plugins.gitalk;
 	if (gitalkConfig) {
-		gitalkConfig.id = data._id;
-
 		data.content = data.content + '\n<div id="gitalk-container"></div>\n<link rel="stylesheet" href="https://unpkg.com/gitalk/dist/gitalk.css"><script src="https://unpkg.com/gitalk/dist/gitalk.min.js"></script>';
-		var str = '<script>new Gitalk(' + JSON.stringify(gitalkConfig) + ').render("gitalk-container")' + '</script>';
+		var str = `
+		<script>
+		var gitalkConfig = ${JSON.stringify(gitalkConfig)};
+	    gitalkConfig.id = location.pathname;
+		var gitalk = new Gitalk(gitalkConfig);
+	    gitalk.render("gitalk-container");
+	    </script>`;
+		
 		data.content = data.content + '\n' + str;
 	}
 
